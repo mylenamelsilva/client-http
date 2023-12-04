9e5645fb572fdc9e771097d585c005703d994ffa
mylena mel <mylenamsfarias@gmail.com>
2023-12-03 21:21:42 -0300

    feat: created client

---
+++ Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Client
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new() { BaseAddress = new Uri("http://localhost:8080") };
+        }
+    }
+}
